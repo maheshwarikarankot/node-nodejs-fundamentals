8690c603e10d5a790c69ebbd0742aead057bd643
maheshwarikarankot <maheshwari635@gmail.com>
2026-03-14 21:03:09 -0500

    made changes into files

--- src/utils/pathResolver.js
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-
-export const resolvePath = (currentDir, targetPath) => {
-  return path.isAbsolute(targetPath) 
-    ? targetPath 
-    : path.resolve(currentDir, targetPath);
-};
